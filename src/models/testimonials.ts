188508821680dd32c2dc0d67c07ffa94bcfe0a47
Matt Litherland <mattsince87@gmail.com>
2022-10-22 10:51:38 +0300

    Fixed testimonials interface

--- src/models/testimonials.ts
+++ src/models/testimonials.ts
@@ -2,7 +2,8 @@ export interface Testimonial {
   id: number;
   authorImg: string;
   authorBrand: string;
-  authorName: string;
+  authorFirstName: string;
+  authorLastName: string;
   authorPos: string;
   text: string;
 }
