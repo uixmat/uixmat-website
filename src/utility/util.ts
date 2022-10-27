10150d7813e8587010fd625f02302ab91067926d
Matt Litherland <mattsince87@gmail.com>
2022-10-27 15:43:38 +0300

    Fixed ReferenceError in function scope

--- src/utility/util.ts
+++ src/utility/util.ts
@@ -1,10 +1,11 @@
-const cursor = document.getElementById("cursor")
 export function cursorHandlerAdd(cursorClassName: any) {
+  const cursor = document.getElementById("cursor")
   if (cursor != null) {
     cursor.classList.add(cursorClassName)
   }
 }
 export function cursorHandlerRemove(cursorClassName: any) {
+  const cursor = document.getElementById("cursor")
   if (cursor != null) {
     cursor.classList.remove(cursorClassName)
   }
